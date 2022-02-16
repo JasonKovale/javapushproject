1f8e14a687ddffc3f50efd90fa8aa1ce3e95717b
jkovale <jmkovale@gmail.com>
2022-02-16 17:02:05 -0500

    java push project

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+
+
+public class HelloWorld {
+
+
+    // java exercise: Create a Github repository and push a new Java project
+
+    //The Java Project Should Include:
+    //- A main method that prints "Hello, world!" to the console
+    //Bonus points:
+    //- Create a method outside of the main method that prints "Hello, world!" to the console.
+    //- The main method should then call (or invoke) the other method to be able to print.
+
+    public static void main(String[] args){
+
+        String nameOfString;
+        nameOfString = "Hello, world!";
+
+        System.out.println(nameOfString); // "Hello, world!"
+
+
+    }
+}
